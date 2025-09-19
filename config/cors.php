77dffff7ff712e12c1d8d3626ee4f093ce77ec88
SHADALKANE-ZEPH\MFARRASMAJID <muhammad.farras@mitratel.co.id>
2025-09-19 17:26:21 +0700

    Update CORS

--- config/cors.php
+++ config/cors.php
@@ -15,13 +15,11 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
-
-    'allowed_origins_patterns' => [],
+    'allowed_origins' => ['https://digital.mitratel.co.id'],
 
     'allowed_headers' => ['*'],
 
